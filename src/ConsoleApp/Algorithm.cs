a5bb1069100aaeb2e07676396268094b7390cf62
Arief Muhammad Lubis <lubisarieff@gmail.com>
2020-12-21 20:59:12 +0700

    Add project files.

---
+++ src/ConsoleApp/Algorithm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+	public static class Algorithm
+	{
+		public static void TestOne(int maxNumbers)
+		{
+			for (int i = 1; i <= maxNumbers; i++)
+			{
+				Console.WriteLine(i);
+				if (i % 3 == 0)
+					Console.WriteLine("Fizz");
+			
+				if (i % 5 == 0)
+					Console.WriteLine("Buzz");
+				
+				if (i % 3 == 0 && i%5 == 0)
+					Console.WriteLine("FizzBuzz");
+			}
+		}
+
+		public static int TestTwo(string s)
+		{
+			int result = -1;
+			
+			for (int i = 0; i < s.Length; i++)
+			{
+				for (int j = i + 1; j < s.Length; j++)
+				{
+					if (s[i] == s[j])
+					{
+						result = i;
+						break;
+					}
+				}
+
+				if (result != -1)
+					break;
+			}
+
+			return result;
+		}
+
+		public static void TestThree(List<int> numbers)
+		{
+			int length = numbers.Count;
+			int maximumNumber = 0;
+			int indexMaximum = 0;
+
+			for (int i = 0; i < length; i++)
+			{
+				if (numbers[i] > maximumNumber)
+				{
+					maximumNumber = numbers[i];
+					indexMaximum = i;
+				}
+			}
+
+			Swap(numbers, indexMaximum, 0);
+			
+			bool isSwap = false;
+			for (int j = 1; j < length; j++)
+			{
+				isSwap = false;
+				for (int index = 1; index < length - 1; index++)
+				{
+					if (numbers[index] > numbers[index + 1])
+					{
+						Swap(numbers, index, index + 1);
+						isSwap = true;
+					}
+				}
+
+				if (!isSwap)
+					break;
+			}
+		}
+
+		private static void Swap(List<int> numbers, int firstIndex, int secondIndex)
+		{
+			int temp = numbers[firstIndex];
+			numbers[firstIndex] = numbers[secondIndex];
+			numbers[secondIndex] = temp;
+		}
+	}
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Algoritm Questions Number 1");
+			TestOne();
+			Console.WriteLine("Algoritm Questions Number 2");
+			TestTwo();
+			Console.WriteLine("Algoritm Questions Number 3");
+			TestThree();
+		}
+
+		static void TestOne()
+		{
+			Algorithm.TestOne(100);
+		}
+
+		static void TestTwo()
+		{
+			string characterOne = "ABBA";
+			string characterTwo = "ABBC";
+
+			Console.WriteLine(characterOne[Algorithm.TestTwo(characterOne)]);
+			Console.WriteLine(characterTwo[Algorithm.TestTwo(characterTwo)]);
+		}
+
+		static void TestThree()
+		{
+			List<int> numbers1 = new List<int>() { 
+				1,3,13,12,2,5
+			};
+			List<int> numbers2 = new List<int>() {
+				1,3,7,8,2,9
+			};
+
+			Algorithm.TestThree(numbers1);
+			numbers1.ForEach(number => Console.Write($" {number}"));
+
+			Console.WriteLine();
+
+			Algorithm.TestThree(numbers2);
+			numbers2.ForEach(number => Console.Write($" {number}"));
+		}
+	}
+}
